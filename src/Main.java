9c4ddfdb72309d7bc1eda4126e4fadaa855cdac6
Seyed Mohammad Mahdi Hatami <hatamik7@gmail.com>
2020-12-08 19:51:17 +0330

    done

---
+++ src/Main.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        MyLinkedList parenthesisList = new MyLinkedList();
+        for (char c : input.toCharArray()) {
+            switch (c) {
+                case '(':
+                case ')':
+                    parenthesisList.insert(c);
+                    break;
+                case 'R':
+                    parenthesisList.goRight();
+                    break;
+                case 'L':
+                    parenthesisList.goLeft();
+                    break;
+                case 'E':
+                    parenthesisList.erase();
+            }
+        }
+        System.out.println(parenthesisList.print());
+        System.out.println(finMaxValid(parenthesisList.print()));
+    }
+
+    public static int finMaxValid(String input) {
+
+        int leftOpen = 0;
+        int rightOpen = 0;
+        char[] inputArray = input.toCharArray();
+        boolean[] badParenthesis = new boolean[inputArray.length];
+        for (int i = 0; i < inputArray.length; i++) {
+            leftOpen += inputArray[i] == '(' ? 1 : -1;
+            if (leftOpen < 0) {
+                badParenthesis[i] = true;
+                leftOpen = 0;
+            }
+            rightOpen += inputArray[inputArray.length - 1 - i] == ')' ? 1 : -1;
+            if (rightOpen < 0) {
+                badParenthesis[inputArray.length - 1 - i] = true;
+                rightOpen = 0;
+            }
+        }
+        int maxLength = 0;
+        int currentLength = 0;
+        for (boolean isBad : badParenthesis) {
+            if (isBad) {
+                maxLength = Math.max(maxLength, currentLength);
+                currentLength = 0;
+            } else {
+                currentLength ++;
+            }
+        }
+//        for (char c : input.toCharArray()) {
+//            if (c == '(') {
+//                openCounter += 1;
+//            }
+//            else { // c = ')'
+//                if (openCounter == 0) {
+//                    maxLength = Math.max(maxLength, currentLength);
+//                    currentLength = 0;
+//                    continue;
+//                } else {
+//                    openCounter -= 1;
+//                    currentLength += 2;
+//
+//                }
+//            }
+//        }
+        return Math.max(maxLength, currentLength);
+
+    }
+}
---
+++ src/MyLinkedList.java
@@ -0,0 +1,94 @@
+public class MyLinkedList {
+    private final Node head;
+    private Node current;
+
+    public MyLinkedList() {
+        this.head = new Node('\0', null, null);
+        this.current = this.head;
+    }
+
+    public void insert(char ch) {
+        Node newNode = new Node(ch, current.getNext(), current);
+        if (current.hasNext())
+            current.getNext().setLast(newNode);
+        current.setNext(newNode);
+        current = newNode;
+    }
+
+    public void erase() {
+        if (current.hasNext())
+            current.getNext().setLast(current.getLast());
+        if (current.hasLast())
+            current.getLast().setNext(current.getNext());
+        goLeft();
+
+    }
+
+    public void goRight() {
+        if (current.hasNext())
+            current = current.getNext();
+    }
+
+    public void goLeft() {
+
+        if (current.hasLast())
+            current = current.getLast();
+    }
+
+    public String print() {
+        Node node = head.getNext();
+        StringBuilder output = new StringBuilder();
+        while (node != null) {
+            output.append(node.getChar());
+            node = node.getNext();
+        }
+        return output.toString();
+    }
+}
+
+class Node {
+    private final char ch;
+    private boolean isNull;
+    private Node next;
+    private Node last;
+
+    public Node(char ch, Node next, Node last) {
+        this.ch = ch;
+        if (ch == '\0')
+            isNull = true;
+        this.next = next;
+        this.last = last;
+    }
+
+    public char getChar() {
+        return ch;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public boolean isNull() {
+        return isNull;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    public void setLast(Node last) {
+        this.last = last;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public boolean hasNext() {
+        return next != null;
+    }
+
+    public boolean hasLast() {
+        return last != null;
+    }
+}
